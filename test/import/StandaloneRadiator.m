69c36c13b40c92eec2e08732ed8cf854fcd3519a
Edmund Widl <edmund.widl@ait.ac.at>
2016-04-28 17:26:12 +0200

    added example for import

---
+++ test/import/StandaloneRadiator.m
@@ -0,0 +1,61 @@
+% Load FMI++ interface.
+fmippPath = getenv( 'MATLAB_FMIPP_ROOT' );
+addpath( genpath( fullfile( fmippPath, 'packages' ) ) );
+
+% Specify the FMU's model name.
+model_name = 'StandaloneRadiator';
+
+% Specify the path to the extracted (unzipped) FMU.
+uri_to_extracted_fmu = 'file:///D:/Development/matlab-fmipp/test/import/StandaloneRadiator';
+
+% Specify the FMU's configuration parameters.
+logging_on = fmippim.fmiFalse();        % Turn verbosity on/off.
+stop_before_event = fmippim.fmiTrue();  % Stop integration directly before an event occurs.
+event_search_precision = 1e-2;          % Set precision for searching for events.
+integrator_type = fmippim.bdf();        % Specify Sundials CVODE solver (Backward Differentiation Formula).
+%integrator_type = fmippim.rk();        % Alternatively, specify Runge-Kutta integrator.
+
+% Load the FMU.
+fmu = fmippim.FMUModelExchangeV1( uri_to_extracted_fmu, model_name, logging_on, stop_before_event, event_search_precision, integrator_type )
+
+% Instantiate the FMU.
+status = fmu.instantiate( 'standalone_radiator1' )
+if status ~= fmippim.fmiOK(); error( 'instantiation not successful' ); end
+
+% Set value of parameter Tlow.
+Tlow = 82.0;
+status = fmu.setRealValue( 'Tlow', Tlow )
+if status ~= fmippim.fmiOK(); error( 'setRealValue not successful' ); end
+
+% Set value of parameter Thigh.
+Thigh = 90.0
+status = fmu.setRealValue( 'Thigh', Thigh )
+if status ~= fmippim.fmiOK(); error( 'setRealValue not successful' ); end
+
+% Initialize the FMU.
+status = fmu.initialize()
+if status ~= fmippim.fmiOK(); error( 'initialzation not successful' ); end
+
+% Specify default step size of one integration step and the internal intergrator step size.
+stepsize = 300;
+integrator_stepsize = stepsize/10;
+
+t = 0;
+tstop = 4 * 60 * 60;
+
+% Simulation loop.
+while t < tstop
+    % Integrate the FMU: try to make a full step, but stop in case an event is detected.
+    t = fmu.integrate( t + stepsize, integrator_stepsize ); % integrate model
+
+    T = fmu.getRealValue( 'T' );        % retrieve value for output variable 'T'
+    derT = fmu.getRealValue( 'derT' );  % retrieve value for output variable 'derT'
+
+    if ( ( abs( T - Thigh ) < 1e-2 ) && ( derT > 0 ) )
+        fmu.setRealValue( 'Pheat', 0.0 );  % turn off heating
+    elseif ( ( abs( T - Tlow ) < 1e-2 ) && ( derT < 0 ) )
+        fmu.setRealValue( 'Pheat', 1e3 );  % turn on heating
+    end
+
+	disp( [ t/3600 T ] );
+end
